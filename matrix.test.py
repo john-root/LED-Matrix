4a5e055043d989b6a40dead0ef4cf370a88911a4
Connor Ballard <connorballard@hotmail.co.uk>
2015-10-09 18:56:49 +0100

    Update matrix.test.py

--- matrix.test.py
+++ matrix.test.py
@@ -1,12 +1,12 @@
 import time
 from neopixel import *
 
-# LED strip configuration:
+# LED grid configuration:
 LED_COUNT      = 96      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 50     # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 50      # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
@@ -26,11 +26,11 @@ def set_shape(x, y):
 		for j in (-1, 0, 1):
 			mode = abs(i)+ abs(j)
 			if mode == 0:
-				set_pixel(xy_to_strip(x+i, y+j, 8), Color(250, 0, 100))
+				set_pixel(xy_to_strip(x+i, y+j, 8), Color(250, 0, 100)) ## The 8 stands for how wide the grid is
 			if mode == 1:
-				set_pixel(xy_to_strip(x+i, y+j, 8), Color(100, 0, 100))	
+				set_pixel(xy_to_strip(x+i, y+j, 8), Color(100, 0, 100))	## The 8 stands for how wide the grid is
 			if mode == 2:
-				set_pixel(xy_to_strip(x+i, y+j, 8), Color(50, 0, 50))
+				set_pixel(xy_to_strip(x+i, y+j, 8), Color(50, 0, 50))   ## The 8 stands for how wide the grid is
 				
 
 strip.begin()
